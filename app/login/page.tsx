22b9cc6f14a49500a4522d9e19efa2785b242a86
Alex Sherman <asherman.ca@gmail.com>
2023-11-16 14:02:34 -0800

    deployment - fixes 2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,11 +12,7 @@ import { useForm, SubmitHandler } from 'react-hook-form'
 import { toast } from 'sonner'
 
 const getURL = () => {
-	let url =
-		(process?.env?.NEXT_PUBLIC_SITE_URL as string) ?? // Set this to your site URL in production env.
-		process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-		'http://localhost:3000/'
-
+	let url = process?.env?.NEXT_PUBLIC_SITE_URL as string
 	// Make sure to include https:// when not localhost.
 	url = url.includes('http') ? url : `https://${url}`
 	// Make sure to including trailing /.
