053f3c510108226f9eea947f355285fa2ae5c4c5
Alex Sherman <asherman.ca@gmail.com>
2023-11-16 15:05:54 -0800

    auth buttons

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,42 +1,52 @@
-import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { Button } from '@nextui-org/react'
+import { createClient } from "@/utils/supabase/server";
+// import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Button, Link } from "@nextui-org/react";
+import { IconLogin, IconLogout } from "@tabler/icons-react";
 
 export default async function AuthButton() {
-	const cookieStore = cookies()
-	const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
-	const {
-		data: { user },
-	} = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	const signOut = async () => {
-		'use server'
+  const signOut = async () => {
+    "use server";
 
-		const cookieStore = cookies()
-		const supabase = createClient(cookieStore)
-		await supabase.auth.signOut()
-		return redirect('/login')
-	}
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
 
-	return user ? (
-		<div className='flex items-center gap-4'>
-			{user.email}
-			<form action={signOut}>
-				{/* <Button /> */}
-				<button className='py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover'>
-					Logout
-				</button>
-			</form>
-		</div>
-	) : (
-		<Link
-			href='/login'
-			className='py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover'
-		>
-			Login
-		</Link>
-	)
+  return user ? (
+    <div className="flex items-center gap-4">
+      {user.email}
+      <form action={signOut}>
+        <Button
+          radius="full"
+          variant="faded"
+          className="p-2"
+          type="submit"
+          isIconOnly
+        >
+          <IconLogout />
+        </Button>
+      </form>
+    </div>
+  ) : (
+    <Button
+      as={Link}
+      radius="full"
+      variant="faded"
+      className="p-2"
+      href="/login"
+      isIconOnly
+    >
+      <IconLogin />
+    </Button>
+  );
 }
