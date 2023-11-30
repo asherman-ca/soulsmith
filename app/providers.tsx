3bdaa1aa7f59fc3218d9eaba3a8a1387571d0773
Alex Sherman <asherman.ca@gmail.com>
2023-11-30 12:49:39 -0800

    likes - homepage functional

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,11 +4,18 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() => createClientComponentClient<any>());
   const router = useRouter();
   return (
     <NextUIProvider navigate={router.push}>
-      <div className="flex min-h-screen flex-col">{children}</div>
+      <SessionContextProvider supabaseClient={supabaseClient}>
+        <div className="flex min-h-screen flex-col">{children}</div>
+      </SessionContextProvider>
     </NextUIProvider>
   );
 }
--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,6 +1,9 @@
+import { createClient } from "@/utils/supabase/client";
 import { cn } from "@nextui-org/react";
+import { useSessionContext } from "@supabase/auth-helpers-react";
 import { IconHeart } from "@tabler/icons-react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 interface LikeButtonProps {
   buildId: number;
@@ -9,14 +12,48 @@ interface LikeButtonProps {
 }
 
 const LikeButton: FC<LikeButtonProps> = ({ buildId, initialLike }) => {
+  const [isLiked, setIsLiked] = useState<boolean | null>(initialLike || null);
+  useEffect(() => {
+    setIsLiked(initialLike);
+  }, [initialLike]);
+
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase,
+  } = useSessionContext();
+
+  const handleLike = async (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (!session?.user) return;
+
+    if (isLiked) {
+      const { error } = await supabase
+        .from("build_likes")
+        .delete()
+        .eq("build", buildId)
+        .eq("user", session.user.id);
+      setIsLiked(false);
+      toast.success("Unliked build");
+    } else {
+      const { error } = await supabase
+        .from("build_likes")
+        .insert({ build: buildId, user: session.user.id });
+      setIsLiked(true);
+      toast.success("Liked build");
+    }
+  };
+  // get supabase user on client
+
   return (
     <button
       className="flex items-center gap-2 rounded-md bg-gray-900 px-4 py-2 hover:bg-gray-900/50"
-      onClick={() => {}}
+      onClick={handleLike}
     >
       <IconHeart
         className={cn("h-5 w-5", {
-          "text-red-500": initialLike,
+          "text-red-500": isLiked,
         })}
       />
       <p>42</p>
