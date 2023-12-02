8f8cf6c8e2b2eec2bad5d8179281b116e2f2575f
Alex Sherman <asherman.ca@gmail.com>
2023-12-01 18:20:54 -0800

    style - delete button gating

--- app/build/[buildId]/components/DeleteButton.tsx
+++ app/build/[buildId]/components/DeleteButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { createClient } from "@/utils/supabase/client";
 import { Button } from "@nextui-org/react";
+import { IconSkull, IconTrash } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
 import { toast } from "sonner";
@@ -30,13 +31,14 @@ const DeleteButton: FC<DeleteButtonProps> = ({ buildId }) => {
         color="warning"
         onClick={handleDelete}
         radius="none"
-        className="bg-bg100 relative rounded-md border"
+        className="bg-bg100 relative flex gap-2 rounded-md border font-medium"
         variant="bordered"
       >
-        <div className="bg-bg100 duration-[5000ms]! absolute left-0 top-0 z-10 flex h-full w-full translate-y-0 items-center justify-center transition-all group-hover:translate-y-[100%]">
-          <p className="">DELETE BUILD</p>
+        <div className="bg-bg100 ease absolute left-0 top-0 z-10 flex h-full w-full translate-y-0 items-center justify-center gap-2 duration-700 group-hover:translate-y-[100%]">
+          DELETE BUILD
+          <IconTrash />
         </div>
-        NO BACKSIES
+        NO BACKSIES <IconSkull />
       </Button>
     </div>
   );
